62d14958edfee2ef75fb77f001603c1f3dd6225a
Kevin Cho <kccho2254@gmail.com>
2021-01-18 23:10:42 -0600

    seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,7 @@
 -- Start mysql server: $ mysql -u root -p
+INSERT INTO burger (name) VALUES ('Baconator');
+INSERT INTO burger (name) VALUES ('Bleu Cheese');
+INSERT INTO burger (name, devoured) VALUES ('Double with Cheese', true);
+INSERT INTO burger (name, devoured) VALUES ('McRib', true);
+INSERT INTO burger (name, devoured) VALUES ('McDouble', true);
+INSERT INTO burger (name) VALUES ('Bison Burger');
