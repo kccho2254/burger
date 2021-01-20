60f3f12d30832bd019a9b03ea3d6047b406954a8
Kevin Cho <kccho2254@gmail.com>
2021-01-19 22:03:49 -0600

    routes, db functions importing orm, imported js to server script

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # burger
 
-Look at CatsApp and just copy paste everything lole
--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.use(express.static(__dirname + 'public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
