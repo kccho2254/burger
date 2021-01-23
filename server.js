ff3f550b0d9687cc7d7daf4b8a532b1036cedb3c
Kevin Cho <kccho2254@gmail.com>
2021-01-22 18:40:40 -0600

    uncommitted changes, started some handlebars, fixed some connection issues, made a public folder for css/js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express = require("express");
+const express = require("express");
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
